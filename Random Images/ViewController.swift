1f1452fe1ff124fb85a7179f8167352e0d9a6188
Ali <ali.cse233@gmail.com>
2022-04-11 16:04:30 +0300

    Initial commit
    First ios learning commit

--- Random Images/ViewController.swift
+++ Random Images/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let img: UIImageView = {
+       
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFill
+        image.backgroundColor = .white
+        
+        return image
+    }()
+    
+    
+    private let btn: UIButton = {
+       
+        let bt = UIButton()
+        bt.backgroundColor = .white
+        bt.setTitle("Get random image", for: .normal)
+        bt.setTitleColor(.black, for: .normal)
+        return bt
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .cyan
+        view.addSubview(img)
+        img.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        img.center = view.center
+        
+        view.addSubview(btn)
+    
+        getRandomImage()
+        
+        btn.addTarget(self, action: #selector(btnTap), for: .touchUpInside)
     }
 
+    @objc func btnTap(){
+        getRandomImage()
+    }
+    
+    override func viewDidLayoutSubviews() {
 
+        btn.frame = CGRect(x: 20, y: view.frame.height-50-view.safeAreaInsets.bottom, width:
+                            view.frame.width-40, height: 50)
+
+    }
+    
+    func getRandomImage() {
+        let url = "https://source.unsplash.com/random/600x600"
+        let uri = URL(string: url)!
+        guard let data = try? Data(contentsOf: uri) else {
+            return
+        }
+            
+            img.image = UIImage(data: data)
+        
+    }
 }
 
